839ed3844427fa73bb8f27c5a5fe263bc2636361
rerost <gmailii@yahoo.co.jp>
2018-09-29 17:51:48 +0900

    Add request test

--- infra/github/api/client_test.go
+++ infra/github/api/client_test.go
@@ -1,8 +1,10 @@
 package api_test
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sync"
 	"testing"
 
 	"github.com/rerost/vgithub-api/infra/github/api"
@@ -17,8 +19,6 @@ func newDummyServer(callback func(w http.ResponseWriter, r *http.Request)) *http
 }
 
 func TestNewClient(t *testing.T) {
-	dummyURL, _ := url.Parse("http://momokuri-rock.com")
-
 	inOutPairs := []struct {
 		test string
 		in   string // Token
@@ -26,7 +26,7 @@ func TestNewClient(t *testing.T) {
 	}{
 		{
 			test: "When passed token",
-			in:   "asd",
+			in:   "TESTTOKEN",
 			out:  "",
 		},
 		{
@@ -39,6 +39,7 @@ func TestNewClient(t *testing.T) {
 	for _, p := range inOutPairs {
 		t.Run(p.test, func(t *testing.T) {
 			dummyServer := newDummyServer(func(w http.ResponseWriter, r *http.Request) {})
+			dummyURL, _ := url.Parse(dummyServer.URL)
 			_, err := api.NewClient(dummyServer.Client(), dummyURL, p.in)
 			var errMsg string
 			if err != nil {
@@ -48,4 +49,41 @@ func TestNewClient(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestRequest(t *testing.T) {
+	dummyToken := "TESTTOKEN"
+
+	inOutPairs := []struct {
+		test  string
+		query string // DummyResponse (== Query)
+	}{
+		{
+			test: "Empty query",
+		},
+	}
+	var wg sync.WaitGroup
+	for _, p := range inOutPairs {
+		wg.Add(1)
+		t.Run(p.test, func(t *testing.T) {
+			defer wg.Done()
+			dummyServer := newDummyServer(func(w http.ResponseWriter, r *http.Request) {
+				defer r.Body.Close()
+				b, _ := ioutil.ReadAll(r.Body)
+				w.Write([]byte(b))
+			})
+			dummyURL, _ := url.Parse(dummyServer.URL)
+			client, _ := api.NewClient(dummyServer.Client(), dummyURL, dummyToken)
+			res, err := client.Request(p.query)
+			if err != nil {
+				t.Errorf("Unexpected error is occured: %s", err)
+				return
+			}
+			body, _ := ioutil.ReadAll(res.Body)
+			if string(body) != p.query {
+				t.Errorf("\nWant: %s\nHave: %s\n", p.query, string(body))
+			}
+		})
+	}
+	wg.Wait()
 }
